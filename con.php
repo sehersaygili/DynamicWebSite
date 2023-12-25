cdad0a44d42afe337392b7c442c948fdf128c464
Seher Saygılı <sehersygl111@gmail.com>
2023-12-25 17:42:51 +0300

    first commit

---
+++ con.php
@@ -0,0 +1,14 @@
+<?php
+    $host = 'localhost:3306'; 
+    $dbname = 'denemedb';
+    $username = 'root';
+    $password = 'root';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Veritabanına bağlantı başarılı.";
+    } catch (PDOException $e) {
+        die("Veritabanına bağlantı sağlanamadı: " . $e->getMessage());
+    }
+?>
